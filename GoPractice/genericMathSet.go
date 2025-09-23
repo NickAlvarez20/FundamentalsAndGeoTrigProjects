e0b9db54183229109ce288df28dbf2a311df4ee2
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2025-09-22 20:39:23 -0700

    Completed genericMathSet designs and understandings

--- GoPractice/genericMathSet.go
+++ GoPractice/genericMathSet.go
@@ -5,45 +5,47 @@ import (
 )
 
 // Type definition
-type Set[E comparable] map[E]struct{}
+type Set[E comparable] struct {
+	elements map[E]struct{}
+}
 
 // Constructor function
-func NewSet[E comparable]() Set[E]{
-	return Set[E]{}
+func NewSet[E comparable]() *Set[E] {
+	return &Set[E]{
+		elements: make(map[E]struct{}),
+	}
 }
 
 // Functions:
-// Add method: Takes some value and stores it in the set 
-func(s Set[E]) Add(v E){
-	s[v] = struct
+// Add method: Takes some value and stores it in the set
+func (s *Set[E]) Add(v E) {
+	s.elements[v] = struct{}{}
 }
 
 // Remove method: Removes an element from the set, no effect if it doesn't exist
-func (s Set[E]) Remove(v E){
-	
+func (s *Set[E]) Remove(v E) {
+	delete(s.elements, v)
 }
 
 // Contains method: Returns true if specified value exists in set, false otherwise
-func (s Set[E]) Contains(v E) bool{
-	_, ok := s[v]
-	return ok
+func (s *Set[E]) Contains(v E) bool {
+	_, found := s.elements[v]
+	return found
 }
 
 // Size method: Return the number of elements within the set
 
-func(s Set[E]) Size(E) int{
-	return len(Set[E])
+func (s *Set[E]) Size() int {
+	return len(s.elements)
 }
-
-
-
-
 
 func main() {
 	s := NewSet[int]()
 	s.Add(1)
 	s.Add(2)
 	s.Add(3)
+	fmt.Println(s.Contains(3)) // true
+	fmt.Println(s.Size()) // 3
 	s.Add(3)
 	s.Remove(2)
 	fmt.Println(s.Contains(1)) // true
